3fd1337302cf2ff601824507a8d79f9cf4c5ed34
Strilanc <craig.gidney@gmail.com>
2013-01-30 03:17:58 -0400

    Added DiscardRedundantWorkThrottle

---
+++ TwistedOakThreading/Threading/DiscardRedundantWorkThrottle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwistedOak.Threading {
+    ///<summary>
+    ///Throttles work until the previous work's task has completed.
+    ///When new work is provided before the previous work's task has completed, any existing throttled work is discarded and cancelled.
+    ///</summary>
+    public sealed class DiscardRedundantWorkThrottle {
+        private sealed class Job {
+            public Func<Task> Work;
+            public TaskCompletionSource CompletionSource;
+        }
+        private static readonly Job RunningButNothingQueuedJob = new Job();
+
+        ///<summary>Holds the interlocked running/qeued state.</summary>
+        ///<remarks>
+        ///if null: not running and nothing queued
+        ///elif RunningButNothingQueuedJob: running something but nothing queued
+        ///elif X -> running something and X queued
+        ///</remarks>
+        private Job _queuedJob;
+        private readonly SynchronizationContext _runContext;
+
+        ///<summary>Constructs a throttle that runs work on the given context.</summary>
+        ///<param name="runContext">
+        ///The synchronization context that work is run on.
+        ///Defaults to the thread pool when null.
+        ///</param>
+        public DiscardRedundantWorkThrottle(SynchronizationContext runContext = null) {
+            this._runContext = runContext ?? new SynchronizationContext();
+        }
+
+        private async void ProcessActionsAsync() {
+            do {
+                // get last set action
+                var n = Interlocked.Exchange(ref _queuedJob, RunningButNothingQueuedJob);
+                // always re-enter the running context, to ensure the throttle doesn't dominate it
+                await _runContext;
+                // await completion of potentially asynchronous work
+                await n.CompletionSource.EventuallySetFromTask(Tasks.FromEvaluation(n.Work).Unwrap());
+                // exit when no queued work was set during the previous work
+            } while (!ReferenceEquals(Interlocked.CompareExchange(ref _queuedJob, null, RunningButNothingQueuedJob), RunningButNothingQueuedJob));
+        }
+
+        ///<summary>
+        ///Sets the next (asynchronous) work to be run, either now or when the currently running action has finished, to be the given function.
+        ///The work will not be considered completed until the resulting task has completed.
+        ///</summary>
+        ///<returns>A task for the work's eventual result, failure, or cancelation (if it is replaced before it can run).</returns>
+        public Task SetNextToAsyncFunction(Func<Task> asyncWork) {
+            if (asyncWork == null) throw new ArgumentNullException("asyncWork");
+            var t = new TaskCompletionSource();
+            var prev = Interlocked.Exchange(ref _queuedJob, new Job {Work = asyncWork, CompletionSource = t});
+            if (prev == null) { // there was no queued work or running work
+                ProcessActionsAsync();
+            } else if (!ReferenceEquals(prev, RunningButNothingQueuedJob)) { // we replaced some queued work
+                prev.CompletionSource.SetCanceled();
+            } // else there's running work and we were the first to queue, and nothing needs to be done
+            return t.Task;
+        }
+
+        ///<summary>
+        ///Sets the next (asynchronous) work to be run, either now or when the currently running action has finished, to be the given function.
+        ///The work will not be considered completed until the resulting task has completed.
+        ///</summary>
+        ///<returns>A task for the work's eventual result, failure, or cancelation (if it is replaced before it can run).</returns>
+        public async Task<T> SetNextToAsyncFunction<T>(Func<Task<T>> func) {
+            if (func == null) throw new ArgumentNullException("func");
+            var result = default(T);
+            await SetNextToAsyncFunction(async () => { result = await func(); });
+            return result;
+        }
+
+        ///<summary>Sets the next work to be run, either now or when the currently running action has finished, to be the given action.</summary>
+        ///<returns>A task for the work's eventual success, failure, or cancelation (if it is replaced before it can run).</returns>
+        public Task SetNextToAction(Action action) {
+            if (action == null) throw new ArgumentNullException("action");
+            return SetNextToAsyncFunction(() => { action(); return Tasks.RanToCompletion(); });
+        }
+
+        ///<summary>Sets the next work to be run, either now or when the currently running action has finished, to be the given function.</summary>
+        ///<returns>A task for the work's eventual result, failure, or cancelation (if it is replaced before it can run).</returns>
+        public Task<T> SetNextToFunction<T>(Func<T> func) {
+            if (func == null) throw new ArgumentNullException("func");
+            return SetNextToAsyncFunction(() => Task.FromResult(func()));
+        }
+    }
+}
---
+++ TwistedOakThreadingTest/Threading/DiscardRedundantWorkThrottleTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwistedOak.Threading;
+
+[TestClass]
+public class DiscardRedundantWorkThrottleTest {
+    [TestMethod]
+    public void SetNext() {
+        var r = new DiscardRedundantWorkThrottle();
+        var ax = new ArgumentException();
+        var cx = new OperationCanceledException();
+
+        // results are propagated
+        var n = 0;
+        r.SetNextToAction(() => n++).AssertRanToCompletion();
+        n.AssertEquals(1);
+        r.SetNextToFunction(() => 2).AssertRanToCompletion().AssertEquals(2);
+        r.SetNextToAsyncFunction(Tasks.RanToCompletion).AssertRanToCompletion();
+        r.SetNextToAsyncFunction(() => Task.FromResult(3)).AssertRanToCompletion().AssertEquals(3);
+
+        // faulted tasks are propagated
+        r.SetNextToAsyncFunction(() => Tasks.Faulted(ax)).AssertFailed<ArgumentException>();
+        r.SetNextToAsyncFunction(() => Tasks.Faulted<int>(ax)).AssertFailed<ArgumentException>();
+
+        // cancelled tasks are propagated
+        r.SetNextToAsyncFunction(Tasks.Cancelled).AssertCancelled();
+        r.SetNextToAsyncFunction(Tasks.Cancelled<int>).AssertCancelled();
+
+        // thrown cancellation exceptions indicate cancellation
+        r.SetNextToAsyncFunction(() => { throw cx; }).AssertCancelled();
+        r.SetNextToAsyncFunction<int>(() => { throw cx; }).AssertCancelled();
+        r.SetNextToAction(() => { throw cx; }).AssertCancelled();
+        r.SetNextToFunction<int>(() => { throw cx; }).AssertCancelled();
+
+        // thrown exceptions are propagated
+        r.SetNextToAsyncFunction(() => { throw ax; }).AssertFailed<ArgumentException>();
+        r.SetNextToAsyncFunction<int>(() => { throw ax; }).AssertFailed<ArgumentException>();
+        r.SetNextToAction(() => { throw ax; }).AssertFailed<ArgumentException>();
+        r.SetNextToFunction<int>(() => { throw ax; }).AssertFailed<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void NoOverlap() {
+        var r = new DiscardRedundantWorkThrottle();
+        var n = 0;
+        Enumerable.Range(0, 5)
+                  .Select(async e => await Task.Factory.StartNew(
+                      () => {
+                          for (var i = 0; i < 1000; i++) {
+                              r.SetNextToAction(() => {
+                                  Interlocked.Increment(ref n).AssertEquals(1);
+                                  Interlocked.Decrement(ref n).AssertEquals(0);
+                              });
+                          }
+                      },
+                      TaskCreationOptions.LongRunning))
+                  .WhenAll()
+                  .AssertRanToCompletion();
+    }
+
+    [TestMethod]
+    public void WaitsForAsyncAndCancels() {
+        var r = new DiscardRedundantWorkThrottle();
+        var h = new TaskCompletionSource();
+        
+        var t1 = r.SetNextToAsyncFunction(() => h.Task);
+        var t2 = r.SetNextToAction(() => { });
+        new[] { t1, t2 }.WhenAny().AssertNotCompleted();
+        
+        var t3 = r.SetNextToAction(() => { });
+        t2.AssertCancelled();
+        new[] {t1, t3}.WhenAny().AssertNotCompleted();
+        
+        h.SetRanToCompletion();
+        t1.AssertRanToCompletion();
+        t3.AssertRanToCompletion();
+    }
+
+    [TestMethod]
+    public void WorksWithReentrancy() {
+        for (var i = 0; i < 25; i++) {
+            var r = new DiscardRedundantWorkThrottle();
+            var h = new TaskCompletionSource();
+            Task t = null;
+            r.SetNextToAction(() => t = r.SetNextToAsyncFunction(() => h.Task)).AssertRanToCompletion();
+            t.AssertNotCompleted(timeout: TimeSpan.FromMilliseconds(5));
+            h.SetRanToCompletion();
+            t.AssertRanToCompletion();
+        }
+    }
+}
